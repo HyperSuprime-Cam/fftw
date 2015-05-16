6e2d45d5f4b92c73e57d6d322fe74339091545d6
Mario Juric <mjuric@lsst.org>
2015-05-16 01:40:38 -0700

    eupspkg.cfg.sh: append to $CONFIG_OPTIONS, don't override

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -1,4 +1,4 @@
 # EupsPkg config file. Sourced by 'eupspkg'
 
-CONFIGURE_OPTIONS="--prefix $PREFIX --disable-fortran --enable-shared"
+CONFIGURE_OPTIONS="$CONFIGURE_OPTIONS --disable-fortran --enable-shared"
 MAKE_INSTALL_TARGETS="-j1 install"

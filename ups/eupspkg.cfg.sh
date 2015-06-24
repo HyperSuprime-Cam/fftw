c48742e01122b9ac9220f2ba3fd9179b8830b979
Nate Lust <nlust@astro.princeton.edu>
2015-06-24 12:45:02 -0400

    updated configure for single and double precision

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -1,4 +1,5 @@
 # EupsPkg config file. Sourced by 'eupspkg'
 
-CONFIGURE_OPTIONS="--prefix $PREFIX --disable-fortran --enable-shared"
+CONFIGURE_OPTIONS="--prefix $PREFIX --disable-fortran --enable-shared
+--enable-single"
 MAKE_INSTALL_TARGETS="-j1 install"
